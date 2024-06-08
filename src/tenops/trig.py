cd26f384d8b10c39d7b7ac9d2bcd19e85b0d4d7f
Brandon Bergerud <brandon.bergerud@gmail.com>
2024-06-08 19:56:44 -0400

    feat: added new functions; utilized AttrHandler

--- src/tenops/trig.py
+++ src/tenops/trig.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from .manage import (
     DEFAULT_MODULE,
     TYPEHINT_DTYPE,
@@ -9,26 +7,55 @@ from .manage import (
     get_module_from_object,
     get_module_from_objects,
 )
+from .utils import AttrHandler
 
 
 def angle(
     x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
 ) -> TYPEHINT_DTYPE:
-    d = defaultdict(lambda: "angle")
-    d["tensorflow"] = "math.angle"
+    a = AttrHandler("angle", tensorflow="math.angle")
     module = get_module_from_object(x, default=default)
     x = cast_to_dtype(x, module=module)
-    return get_module_attr(module, d[module])(x, **kwargs)
+    return get_module_attr(module, a[module])(x, **kwargs)
+
+
+def acos(
+    x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
+) -> TYPEHINT_DTYPE:
+    a = AttrHandler("acos", numpy="arccos")
+    module = get_module_from_object(x, default=default)
+    x = cast_to_dtype(x, module=module)
+    return get_module_attr(module, a[module])(x, **kwargs)
+
+
+def acosh(x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs):
+    a = AttrHandler("acosh", numpy="arccosh")
+    module = get_module_from_object(x, default=default)
+    x = cast_to_dtype(x, module=module)
+    return get_module_attr(module, a[module])(x, **kwargs)
+
+
+def asin(x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs):
+    a = AttrHandler("asin", numpy="arcsin")
+    module = get_module_from_object(x, default=default)
+    x = cast_to_dtype(x, module=module)
+    return get_module_attr(module, a[module])(x, **kwargs)
+
+
+def asinh(x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs):
+    a = AttrHandler("asinh", numpy="arcsinh")
+    module = get_module_from_object(x, default=default)
+    x = cast_to_dtype(x, module=module)
+    return get_module_attr(module, a[module])(x, **kwargs)
 
 
 def atan(
     x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
 ) -> TYPEHINT_DTYPE:
-    d = defaultdict(lambda: "arctan")
-    d["tensorflow"] = "atan"
+    a = AttrHandler("atan", numpy="arctan")
     module = get_module_from_object(x, default=default)
     x = cast_to_dtype(x, module=module)
-    return get_module_attr(module, d[module])(x, **kwargs)
+    return get_module_attr(module, a[module])(x, **kwargs)
 
 
 def atan2(
@@ -37,41 +64,54 @@ def atan2(
     default: TYPEHINT_MODULE = DEFAULT_MODULE,
     **kwargs
 ) -> TYPEHINT_DTYPE:
-    d = defaultdict(lambda: "arctan2")
-    d["tensorflow"] = "atan2"
+    a = AttrHandler("atan2", numpy="arctan2")
     module = get_module_from_objects([x, y], default=default)
     x = cast_to_dtype(x, module=module)
     y = cast_to_dtype(y, module=module)
-    return get_module_attr(module, d[module])(y, x, **kwargs)
+    return get_module_attr(module, a[module])(y, x, **kwargs)
+
+
+def atanh(
+    x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
+) -> TYPEHINT_DTYPE:
+    a = AttrHandler("atanh", numpy="arctanh")
+    module = get_module_from_object(x, default=default)
+    x = cast_to_dtype(x, module=module)
+    return get_module_attr(module, a[module])(x, **kwargs)
 
 
 def cos(
     x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
 ) -> TYPEHINT_DTYPE:
-    d = defaultdict(lambda: "cos")
+    a = AttrHandler("cos")
     module = get_module_from_object(x, default=default)
     x = cast_to_dtype(x, module=module)
-    return get_module_attr(module, d[module])(x, **kwargs)
+    return get_module_attr(module, a[module])(x, **kwargs)
 
 
 def sin(
     x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
 ) -> TYPEHINT_DTYPE:
-    d = defaultdict(lambda: "sin")
+    a = AttrHandler("sin")
     module = get_module_from_object(x, default=default)
     x = cast_to_dtype(x, module=module)
-    return get_module_attr(module, d[module])(x, **kwargs)
+    return get_module_attr(module, a[module])(x, **kwargs)
 
 
 def tan(
     x: TYPEHINT_DTYPE, default: TYPEHINT_MODULE = DEFAULT_MODULE, **kwargs
 ) -> TYPEHINT_DTYPE:
-    d = defaultdict(lambda: "tan")
+    a = AttrHandler("tan")
     module = get_module_from_object(x, default=default)
     x = cast_to_dtype(x, module=module)
-    return get_module_attr(module, d[module])(x, **kwargs)
+    return get_module_attr(module, a[module])(x, **kwargs)
 
 
 # Aliases
+arccos = acos
+arccosh = acosh
+arcsin = asin
+arcsinh = asinh
+arctan = atan
 arctan = atan
 arctan2 = atan2
--- tests/test_trig.py
+++ tests/test_trig.py
@@ -1,3 +1,4 @@
+import cmath
 import math
 
 from tenops import trig
@@ -12,18 +13,51 @@ def test_angle():
         assert abs(trig.angle(cast_to_dtype(x, module)) - angle) < 1e-6, module
 
 
+def test_acos():
+    x = 0.5
+    y = math.acos(x)
+    for module in MODULE_TYPECAST.keys():
+        assert abs(trig.acos(x, default=module) - y) < 1e-6, module
+        assert abs(trig.acos(cast_to_dtype(x, module)) - y) < 1e-6, module
+
+
+def test_acosh():
+    x = 1 + 1j
+    y = cmath.log(x + cmath.sqrt(x - 1) * cmath.sqrt(x + 1))
+
+    for module in MODULE_TYPECAST.keys():
+        assert abs(trig.acosh(x, default=module) - y) < 1e-6, module
+        assert abs(trig.acosh(cast_to_dtype(x, module)) - y) < 1e-6, module
+
+
+def test_asin():
+    x = 1.0
+    y = math.asin(x)
+    for module in MODULE_TYPECAST.keys():
+        assert abs(trig.asin(x, default=module) - y) < 1e-6, module
+        assert abs(trig.asin(cast_to_dtype(x, module)) - y) < 1e-6, module
+
+
+def test_asinh():
+    x = 1 + 1j
+    y = cmath.asinh(x)
+    for module in MODULE_TYPECAST.keys():
+        assert abs(trig.asinh(x, default=module) - y) < 1e-6, module
+        assert abs(trig.asinh(cast_to_dtype(x, module)) - y) < 1e-6, module
+
+
 def test_atan():
     x = 1.0
-    angle = math.pi / 4
+    y = math.atan(x)
     for module in MODULE_TYPECAST.keys():
-        assert abs(trig.atan(x, default=module) - angle) < 1e-6, module
-        assert abs(trig.atan(cast_to_dtype(x, module)) - angle) < 1e-6, module
+        assert abs(trig.atan(x, default=module) - y) < 1e-6, module
+        assert abs(trig.atan(cast_to_dtype(x, module)) - y) < 1e-6, module
 
 
 def test_atan2():
     x = 1.0
     y = -1.0
-    angle = -math.pi / 4
+    angle = math.atan2(y, x)
 
     for module in MODULE_TYPECAST.keys():
         assert abs(trig.atan2(y=y, x=x, default=module) - angle) < 1e-6, module
@@ -32,9 +66,17 @@ def test_atan2():
         assert abs(trig.atan2(y=ym, x=xm) - angle) < 1e-6, module
 
 
+def test_atanh():
+    x = 1 + 1j
+    y = cmath.atanh(x)
+    for module in MODULE_TYPECAST.keys():
+        assert abs(trig.atanh(x, default=module) - y) < 1e-6, module
+        assert abs(trig.atanh(cast_to_dtype(x, module)) - y) < 1e-6, module
+
+
 def test_cos():
     x = math.pi / 3
-    y = 0.5
+    y = math.cos(x)
     for module in MODULE_TYPECAST.keys():
         assert abs(trig.cos(x, default=module) - y) < 1e-6, module
         assert abs(trig.cos(cast_to_dtype(x, module)) - y) < 1e-6, module
@@ -42,7 +84,7 @@ def test_cos():
 
 def test_sin():
     x = math.pi / 6
-    y = 0.5
+    y = math.sin(x)
     for module in MODULE_TYPECAST.keys():
         assert abs(trig.sin(x, default=module) - y) < 1e-6, module
         assert abs(trig.sin(cast_to_dtype(x, module)) - y) < 1e-6, module
@@ -50,7 +92,7 @@ def test_sin():
 
 def test_tan():
     x = math.pi / 4
-    y = 1.0
+    y = math.tan(x)
     for module in MODULE_TYPECAST.keys():
         assert abs(trig.tan(x, default=module) - y) < 1e-6, module
         assert abs(trig.tan(cast_to_dtype(x, module)) - y) < 1e-6, module
