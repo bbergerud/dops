d4c876b63863cef5d74fe623947c9f2d09110f51
Brandon Bergerud <brandon.bergerud@gmail.com>
2024-06-08 19:48:27 -0400

    feat: created attr and module handlers; renamed parameter handler

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,19 @@
-from tenops.utils import ParameterAlias
+from tenops.utils import AttrHandler, ModuleHandler, ParameterHandler
 
 
-def test_Parameter():
-    p = ParameterAlias(params=dict(axis=1), axis=dict(torch="dim"))
+def test_AttrHandler():
+    a = AttrHandler("asin", numpy="arcsin")
+    assert a["torch"] == "asin"
+    assert a["numpy"] == "arcsin"
+
+
+def test_ModuleHandler():
+    m = ModuleHandler(test="not test")
+    assert m["torch"] == "torch"
+    assert m["test"] == "not test"
+
+
+def test_ParameterHandler():
+    p = ParameterHandler(params=dict(axis=1), axis=dict(torch="dim"))
+    assert p["numpy"] == {"axis": 1}
     assert p["torch"] == {"dim": 1}
